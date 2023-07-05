ffc6fa5886211e714d1ac97ab30ab2e1f18ecd0c
mppanayotov <m.p.panayotov@gmail.com>
2023-07-05 09:58:27 +0300

    Update Login-page styles; Add copy button to dialog-view

--- libs/record-list/dialog-view-record/src/lib/record-list-dialog-view-record.module.ts
+++ libs/record-list/dialog-view-record/src/lib/record-list-dialog-view-record.module.ts
@@ -5,6 +5,9 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 
 @NgModule({
   imports: [
@@ -13,6 +16,9 @@ import { MatButtonModule } from '@angular/material/button';
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
+    MatIconModule,
+    MatTooltipModule,
+    ClipboardModule,
   ],
   declarations: [DialogViewRecordComponent],
   exports: [DialogViewRecordComponent],
