b96b989acb4e85d361453e8b861cce057a8f73e6
Milen Panayotov <m.p.panayotov@gmail.com>
2024-05-27 17:36:56 +0300

    Add Next app; Add Vite; Update package .json dependencies

---
+++ apps/capital-next/server/main.ts
@@ -0,0 +1,46 @@
+/*
+ * This is only a minimal custom server to get started.
+ * You may want to consider using Express or another server framework, and enable security features such as CORS.
+ *
+ * For an example, see the Next.js repo:
+ * Node - https://github.com/vercel/next.js/blob/canary/examples/custom-server
+ */
+import { createServer } from 'http';
+import { parse } from 'node:url';
+import * as path from 'path';
+import next from 'next';
+
+// Next.js server options:
+// - The environment variable is set by `@nx/next:server` when running the dev server.
+// - The fallback `__dirname` is for production builds.
+// - Feel free to change this to suit your needs.
+
+const dir = process.env.NX_NEXT_DIR || path.join(__dirname, '../../../../apps/capital-next') 
+const dev = process.env.NODE_ENV === 'development';
+
+// HTTP Server options:
+// - Feel free to change this to suit your needs.
+const hostname = process.env.HOST || 'localhost';
+const port = process.env.PORT ? parseInt(process.env.PORT) : 4200;
+
+async function main() {
+  const nextApp = next({ dev, dir });
+  const handle = nextApp.getRequestHandler();
+
+  await nextApp.prepare();
+
+  const server = createServer((req, res) => {
+    const parsedUrl = parse(req.url ?? '', true);
+    handle(req, res, parsedUrl);
+  });
+
+  server.listen(port, hostname);
+
+  console.log(`[ ready ] on http://${hostname}:${port}`)
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
+
