2f810bd732ac15c446e19bf9c61c45081e67052c
Milen Panayotov <140506620+milenpanayotov@users.noreply.github.com>
2024-09-03 02:48:14 +0300

    Update header.component.ts

--- libs/shared/header/src/lib/header/header.component.ts
+++ libs/shared/header/src/lib/header/header.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatNavList } from '@angular/material/list';
+import { Router } from '@angular/router';
 import { AuthService } from '@capital/services/auth-service';
 
 @Component({
-  selector: 'capital-header',
-  templateUrl: './header.component.html',
+	@@ -7,10 +9,17 @@ import { AuthService } from '@capital/services/auth-service';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  @ViewChild(MatNavList) navList!: MatNavList;
+
+  opened = false;
   role = this.authService.role();
   expirationTimer$ = this.authService.expirationTimer();
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
+
+  setActiveLink(link: string): boolean {
+    return link === this.router.url;
+  }
 
   logout(): void {
     this.authService.logout();
